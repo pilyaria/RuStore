ca5db082c21ca879de3dc7249f6172bf37f6703b
pilyaria <pilyar@teplo-inform.ru>
2025-11-28 00:29:59 +0300

    Create AppDetailsState.kt

---
+++ app/src/main/java/io/mmaltsev/vkeducation/presentation/appdetails/AppDetailsState.kt
@@ -0,0 +1,14 @@
+package io.mmaltsev.vkeducation.presentation.appdetails
+
+import androidx.compose.runtime.Immutable
+import io.mmaltsev.vkeducation.domain.appdetails.AppDetails
+
+@Immutable
+sealed interface AppDetailsState {
+    data object Error : AppDetailsState
+    data object Loading : AppDetailsState
+    data class Content(
+        val appDetails: AppDetails,
+        val descriptionCollapsed: Boolean,
+    ) : AppDetailsState
+}
