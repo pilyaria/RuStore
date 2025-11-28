e3ad66b980510fe57875a69ce73353b0ac64ecb0
pilyaria <pilyar@teplo-inform.ru>
2025-11-28 22:10:31 +0300

    Update AppDetailsEvent.kt
    installed/uninstalled

--- app/src/main/java/io/mmaltsev/vkeducation/presentation/appdetails/AppDetailsEvent.kt
+++ app/src/main/java/io/mmaltsev/vkeducation/presentation/appdetails/AppDetailsEvent.kt
@@ -2,4 +2,6 @@ package io.mmaltsev.vkeducation.presentation.appdetails
 
 sealed interface AppDetailsEvent {
     data object UnderDevelopment : AppDetailsEvent
+    data class Installed(val appName: String) : AppDetailsEvent
+    data class Uninstalled(val appName: String) : AppDetailsEvent
 }
