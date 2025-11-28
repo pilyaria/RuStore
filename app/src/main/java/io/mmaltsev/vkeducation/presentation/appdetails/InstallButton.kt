fa47512e6569eaffd4321499874c5c2686d8b1c6
pilyaria <pilyar@teplo-inform.ru>
2025-11-28 22:11:45 +0300

    Update InstallButton.kt
    install|uninstall btn

--- app/src/main/java/io/mmaltsev/vkeducation/presentation/appdetails/InstallButton.kt
+++ app/src/main/java/io/mmaltsev/vkeducation/presentation/appdetails/InstallButton.kt
@@ -15,16 +15,18 @@ import io.mmaltsev.vkeducation.presentation.theme.VkEducationTheme
 
 @Composable
 fun InstallButton(
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
+    installed: Boolean,
+    onInstallClick: () -> Unit,
+    onUninstallClick: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
     Button(
-        onClick = onClick,
-        shape = RoundedCornerShape(8.dp),
-        contentPadding = PaddingValues(vertical = 12.dp),
-        modifier = modifier,
+        onClick = {
+            if (installed) onUninstallClick() else onInstallClick()
+        },
+        modifier = modifier
     ) {
-        Text(text = stringResource(R.string.install))
+        Text(if (installed) "Удалить" else "Установить")
     }
 }
 
@@ -33,8 +35,10 @@ fun InstallButton(
 private fun Preview() {
     VkEducationTheme {
         InstallButton(
-            onClick = {},
-            modifier = Modifier.fillMaxWidth(),
+            installed = false,
+            onInstallClick = {},
+            onUninstallClick = {},
+            modifier = Modifier.fillMaxWidth()
         )
     }
 }
